e5647b4cd31008b8b4a7391b51f4f6362d5b5461
Kate Bronstad <kate.bronstad@tufts.edu>
2020-04-04 12:32:15 -0400

    Updates

--- README.md
+++
@@ -1,6 +0,0 @@
-Made with [Create React App](https://github.com/facebook/create-react-app).
-
-## App to help manage book club 
-
-Will eventually make some process of picking & managing books to read for book club easier.
-
---
+++ precache-manifest.cf9e4c04744b1cced7f1dd0c704dab76.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "108289b946bdfeecbcd5e6879565a94f",
+    "url": "/rclass_books/index.html"
+  },
+  {
+    "revision": "88026d791fdab6d67322",
+    "url": "/rclass_books/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d18e046d835a584919ce",
+    "url": "/rclass_books/static/js/2.c81a58c9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/rclass_books/static/js/2.c81a58c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88026d791fdab6d67322",
+    "url": "/rclass_books/static/js/main.ebd47ab3.chunk.js"
+  },
+  {
+    "revision": "e9bc97987d96c4cf7fd6",
+    "url": "/rclass_books/static/js/runtime-main.bcbabcba.js"
+  }
+]);
