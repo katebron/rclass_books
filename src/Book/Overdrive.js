7af393fb483e57a736e9d8f07c5e48fd0285729e
Kate Bronstad <kate.bronstad@tufts.edu>
2020-04-18 18:29:49 -0400

    succesfully return token

--- src/Book/Overdrive.js
+++ src/Book/Overdrive.js
@@ -1,9 +1,44 @@
 import React, { useState } from 'react';
 
 
-const Overdrive = ({isbns}) =>{
-    
-    console.log("this is isbns", isbns)
+const Overdrive = ({book}) =>{
+    var myHeaders = new Headers();
+myHeaders.append("Content-Type", "application/x-www-form-urlencoded;charset=UTF-8");
+myHeaders.append("Authorization", "Basic S0FURUJST05NQTpCelczMXVEUmNlTVRLbDZn");
+
+var raw = "grant_type=client_credentials";
+
+var requestOptions = {
+  method: 'POST',
+  headers: myHeaders,
+  body: raw,
+  redirect: 'follow'
+};
+
+fetch("https://cors-anywhere.herokuapp.com/https://oauth.overdrive.com/token", requestOptions)
+  .then(response => response.text())
+  .then(result => console.log(result))
+  .catch(error => console.log('error', error));
+    /*fetch(process.env.REACT_APP_OVERDRIVE_TOKEN_URL,{ method: 'POST',
+	headers: {
+        'Authorization': `Basic ${process.env.REACT_APP_OVERDRIVE_SECRET}`,
+		'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
+    },
+    mode: 'cors',
+    body: 'grant_type=client_credentials',
+   }).then(function (data) {
+
+	// Log the API data
+	console.log('token', data);
+
+   }).catch(function (err) {
+
+	// Log any errors
+	console.log('something went wrong', err);
+
+  });*/
+
+   // console.log("this is isbns", isbns)
     return (
      <>Library stuff here</>  
     )
